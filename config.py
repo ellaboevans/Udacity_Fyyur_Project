72137536487f8a330dcefd793dcc51fec146557e
eelabo <ellaboevans@gmail.com>
2022-08-07 20:21:53 +0000

    rename dialect

--- config.py
+++ config.py
@@ -5,10 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
 DEBUG = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 # Connect to the database
-
-
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:Erosion123@localhost:5432/fyyur'
-SQLALCHEMY_TRACK_MODFICATIONS ='True'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Erosion123@localhost:5432/fyyur'
